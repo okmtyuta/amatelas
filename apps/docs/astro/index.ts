53990c65463b9cad9fb61c61c42ae76ef2bafeaa
okmtyuta <okmtyuta@gmail.com>
2024-02-12 19:38:13 +0900

    astro docs prebuild

--- apps/docs/astro/index.ts
+++
@@ -1,5 +0,0 @@
-export { Frame, Heading, Paragraph } from './src/components/display'
-export { Ripple } from './src/components/effect'
-export { Button } from './src/components/input'
-export { Positional } from './src/components/layout'
-
